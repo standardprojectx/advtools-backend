0b378fe789f33a69009325133bd3739c6e503401
Standard Projectx <standardprojectx@gmail.com>
2024-09-14 18:49:01 -0300

    feat: updated urls

--- src/controllers/audioController.js
+++ src/controllers/audioController.js
@@ -1,4 +1,5 @@
 const audioService = require('../services/audioService');
+const fs = require('fs');
 
 exports.convertAudio = async (req, reply) => {
   const files = req.files;
@@ -18,17 +19,24 @@ exports.convertAudio = async (req, reply) => {
     reply.raw.on('finish', cleanup);
 
     if (result.type === 'single') {
-      // Enviar o arquivo único
-      reply
+      // Verifique se o arquivo existe
+      if (fs.existsSync(result.file.path)) {
+        reply
         .header('Content-Type', 'application/octet-stream')
         .header('Content-Disposition', `attachment; filename="${result.file.name}"`)
-        .send(fs.createReadStream(result.file.path));
+        .send(fs.createReadStream(result.file.path));      
+      } else {
+        reply.status(500).send({ message: 'Arquivo não encontrado após conversão.' });
+      }
     } else if (result.type === 'zip') {
-      // Enviar o arquivo zip
-      reply
+      if (fs.existsSync(result.path)) {
+        reply
         .header('Content-Type', 'application/zip')
         .header('Content-Disposition', `attachment; filename="${result.name}"`)
-        .send(fs.createReadStream(result.path));
+        .send(fs.createReadStream(result.path));      
+      } else {
+        reply.status(500).send({ message: 'Arquivo zip não encontrado após conversão.' });
+      }
     }
   } catch (error) {
     console.error('Erro ao converter áudio:', error);
