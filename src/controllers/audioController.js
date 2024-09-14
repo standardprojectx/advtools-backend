c08fd3a5332079f3efba6b884717a12291f158cf
Standard Projectx <standardprojectx@gmail.com>
2024-09-14 18:30:22 -0300

    feat: audio controller updated

--- src/controllers/audioController.js
+++ src/controllers/audioController.js
@@ -1,13 +1,35 @@
-const audioService = require('../services/audioService');
-
-exports.convertAudio = async (req, res) => {
+exports.convertAudio = async (req, reply) => {
   const files = req.files;
   const conversionType = req.body.conversionType;
 
   try {
-    await audioService.convertAudioVideo(files, conversionType, res);
+    const result = await audioService.convertAudioVideo(files, conversionType);
+
+    const cleanup = () => {
+      if (result.type === 'single') {
+        fs.unlinkSync(result.file.path);
+      } else if (result.type === 'zip') {
+        fs.unlinkSync(result.path);
+      }
+    };
+
+    reply.raw.on('finish', cleanup);
+
+    if (result.type === 'single') {
+      // Enviar o arquivo único
+      reply
+        .header('Content-Type', 'application/octet-stream')
+        .header('Content-Disposition', `attachment; filename="${result.file.name}"`)
+        .send(fs.createReadStream(result.file.path));
+    } else if (result.type === 'zip') {
+      // Enviar o arquivo zip
+      reply
+        .header('Content-Type', 'application/zip')
+        .header('Content-Disposition', `attachment; filename="${result.name}"`)
+        .send(fs.createReadStream(result.path));
+    }
   } catch (error) {
     console.error('Erro ao converter áudio:', error);
-    res.status(500).send({ message: 'Erro ao converter áudio.' });
+    reply.status(500).send({ message: 'Erro ao converter áudio.' });
   }
 };
