7293f6c64483cfaf5db1073e3c57c81a5e46236a
Standard Projectx <standardprojectx@gmail.com>
2024-09-14 18:25:09 -0300

    feat: just testing what i did in production

--- src/services/audioService.js
+++ src/services/audioService.js
@@ -2,33 +2,72 @@ const ffmpeg = require('fluent-ffmpeg');
 const path = require('path');
 const fs = require('fs');
 const { generateRandomFileName } = require('../utils/utils');
+const archiver = require('archiver');
 
-exports.convertAudioVideo = (files, conversionType, res) => {
+exports.convertAudioVideo = async (files, conversionType) => {
   const outputFiles = [];
 
-  files.forEach(file => {
-    const randomFileName = generateRandomFileName(file.originalname);
-    const outputPath = path.join('/tmp', randomFileName);
+  const conversionPromises = files.map(file => {
+    return new Promise((resolve, reject) => {
+      const outputExtension = conversionType.split('To')[1].toLowerCase();
+      const baseName = path.parse(file.originalname).name;
+      const randomFileName = generateRandomFileName(`${baseName}.${outputExtension}`);
+      const outputPath = path.join('/tmp', randomFileName);
 
-    ffmpeg(file.path)
-      .toFormat(conversionType.split('To')[1].toLowerCase())
-      .on('end', () => {
-        outputFiles.push(outputPath);
-        if (outputFiles.length === files.length) {
-          res.download(outputPath, randomFileName, (err) => {
-            if (err) {
-              console.error('Erro ao enviar o arquivo:', err);
-              res.status(500).send({ message: 'Erro ao enviar o arquivo.' });
-            }
-            fs.unlinkSync(outputPath);
-            files.forEach(file => fs.unlinkSync(file.path));
-          });
-        }
-      })
-      .on('error', (err) => {
-        console.error('Erro ao converter arquivo:', err);
-        res.status(500).send({ message: 'Erro ao converter arquivos.' });
-      })
-      .save(outputPath);
+      ffmpeg(file.path)
+        .toFormat(outputExtension)
+        .on('end', () => {
+          // Remover o arquivo de entrada
+          fs.unlinkSync(file.path);
+          // Adicionar o arquivo de saída à lista
+          outputFiles.push({ path: outputPath, name: randomFileName });
+          resolve();
+        })
+        .on('error', (err) => {
+          console.error('Erro ao converter arquivo:', err);
+          fs.unlinkSync(file.path);
+          reject(err);
+        })
+        .save(outputPath);
+    });
   });
+
+  await Promise.all(conversionPromises);
+
+  if (outputFiles.length === 1) {
+    // Apenas um arquivo, retornar diretamente
+    const outputFile = outputFiles[0];
+    return { type: 'single', file: outputFile };
+  } else {
+    // Múltiplos arquivos, criar um zip
+    const zipFileName = generateRandomFileName('converted_files.zip');
+    const zipFilePath = path.join('/tmp', zipFileName);
+
+    return new Promise((resolve, reject) => {
+      const output = fs.createWriteStream(zipFilePath);
+      const archive = archiver('zip', {
+        zlib: { level: 9 }
+      });
+
+      output.on('close', () => {
+        // Remover os arquivos de saída individuais
+        outputFiles.forEach(file => fs.unlinkSync(file.path));
+        resolve({ type: 'zip', path: zipFilePath, name: zipFileName });
+      });
+
+      archive.on('error', (err) => {
+        console.error('Erro ao criar o arquivo zip:', err);
+        outputFiles.forEach(file => fs.unlinkSync(file.path));
+        reject(err);
+      });
+
+      archive.pipe(output);
+
+      outputFiles.forEach(file => {
+        archive.file(file.path, { name: file.name });
+      });
+
+      archive.finalize();
+    });
+  }
 };
