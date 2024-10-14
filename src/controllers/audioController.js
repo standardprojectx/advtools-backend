dc92ccaad6aedd567ac070fd6830bba13ffe644a
Standard Projectx <standardprojectx@gmail.com>
2024-10-13 23:08:16 -0300

    feat: file convertion

--- src/controllers/audioController.js
+++ src/controllers/audioController.js
@@ -1,13 +1,15 @@
 const audioService = require('../services/audioService');
 const fs = require('fs');
+const path = require('path');
 
 exports.convertAudio = async (req, reply) => {
   const files = req.files;
   const conversionType = req.body.conversionType;
 
-
   try {
     const result = await audioService.convertAudioVideo(files, conversionType);
+
+
     const cleanup = () => {
       if (result.type === 'single') {
         fs.unlinkSync(result.file.path);
@@ -17,26 +19,34 @@ exports.convertAudio = async (req, reply) => {
     };
 
     reply.raw.on('finish', cleanup);
+    
 
     if (result.type === 'single') {
-
       if (fs.existsSync(result.file.path)) {
-        console.log(`Enviando arquivo: ${result.file.path}`);
+
+        const fileBuffer = fs.readFileSync(result.file.path);
+        const stat = fs.statSync(result.file.path);
         reply
-          .header('Content-Type', 'application/octet-stream')
+          .header('Content-Type', 'application/ogg')
           .header('Content-Disposition', `attachment; filename="${result.file.name}"`)
-          .send(fs.createReadStream(result.file.path));
+          .header('Content-Length', stat.size)
+          .send(fileBuffer);
       } else {
         console.error(`Arquivo não encontrado: ${result.file.path}`);
         reply.status(500).send({ message: 'Arquivo não encontrado após conversão.' });
       }
     } else if (result.type === 'zip') {
       if (fs.existsSync(result.path)) {
+ 
+        const fileBuffer = fs.readFileSync(result.path);
+        const stat = fs.statSync(result.path);
         reply
-        .header('Content-Type', 'application/zip')
-        .header('Content-Disposition', `attachment; filename="${result.name}"`)
-        .send(fs.createReadStream(result.path));      
+          .header('Content-Type', 'application/zip')
+          .header('Content-Disposition', `attachment; filename="${result.name}"`)
+          .header('Content-Length', stat.size)
+          .send(fileBuffer);      
       } else {
+        console.error(`Arquivo zip não encontrado: ${result.path}`);
         reply.status(500).send({ message: 'Arquivo zip não encontrado após conversão.' });
       }
     }
