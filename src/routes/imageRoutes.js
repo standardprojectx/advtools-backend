dbb38068175c9ab1f8cc1a7c493f492a6009091f
Standard Projectx <standardprojectx@gmail.com>
2024-10-14 23:47:27 -0300

    feat: changes on routes

--- src/routes/imageRoutes.js
+++ src/routes/imageRoutes.js
@@ -1,7 +1,9 @@
 const imageController = require('../controllers/imageController');
 
 async function imageRoutes(fastify, options) {
-  fastify.post('/convert/image', { preHandler: fastify.uploads.array('files') }, imageController.convertImageToPdf);
+  fastify.post('/convert/image', {
+    preValidation: fastify.uploads.array('files')
+  }, imageController.convertImageToPdf);
 }
 
 module.exports = imageRoutes;
